84defa36800dcfe2dced0ba59d14d8628761a2c9
LordBoots <hdromgool@gmail.com>
2024-12-05 14:22:32 +1300

    Update Opener.js

--- Info/Shaders/Opener.js
+++ Info/Shaders/Opener.js
@@ -1,50 +1,31 @@
 
-// Welcome to the Phaser Code Library!
+// Welcome to the Shader Code Library!
 
 
-// This interactive library provides a collection of reusable code snippets and components for Phaser game development.
-// My aim to to make it easier for rosebud users to grab libraries and code snippets other users and myself would like
-// to make availaibe to other users. 
-// I think this could be a powerful system for finding premade solutions to everyday problems and even some more complex
-// one that are here just for the sake of sharing.
+// This interactive library allows you to view, edit and run shader code examples. 
+// All chader examlples found under the ThreeJS category can be used directly within existing ThreeJs Rosebud Games.
 
-// Most scripts should be documented and ready to use in your Phaser projects althoug some are not finished enough to be
-// documented yet. Some are not finished and will be clearly labeled so with a symbol.  
-
-// Browse, copy, and integrate!
-// Get started by exploring the categories in the library panel.
+// Browse, edit, copy, and integrate!
+// Get started by exploring the categories in the left panel.
 
 // Quick Guide:
-// - Browse categories on the left panel
-// - Click '+' to expand categories and subcategories
-// - Click on any script to view its code
-// - Use 'Copy' button to copy code to clipboard
-// - 'Fold/Unfold All' buttons to collapse/expand code sections
-// - 'Undo/Redo' buttons to manage your edits
-// - Press 'L' key to toggle between vertical and horizontal layouts
-// - Some script can be run by clicking the 'Load script' button in the code view - 
-//   these sxripts are dictated by symbols next to them.
+// - Browse categories on the left panel.
+// - Click '+' to expand categories and subcategories.
+// - Click on any script to view its code.
+// - Use the 'Copy' button in the Code Viewer to copy code to clipboard.
+// - 'Fold All/Unfold All' buttons to collapse/expand code.
+// - 'Undo/Redo' buttons to manage your edits (Keypresses also work).
+// - Click 'Launch script' to launch the script in the editor in it's current state.
 
 // Symbol Legend:
-// - Controller 🎮 - ThreeJS executable script.
-// - Joystick 🕹️ - Phaser executable script.
-// - Rainbow 🌈 - GLSL Shader executable.
-
-
-// There is a section for tips on inetegration practices in the "How to" section of the code library. I would highly suggest if you're
-// not code savy that you read though that section as it will give code you copy over the best chances of success.
-// I will try to provide working demos of most of the libraries and components I post - there will be tips on how to manage this
-// in the "How to" section
-// There is a category for documentation at the bottom of the categories list (There is currently nothing in it).
+// - Rainbow 🌈 - GLSL Shader executable that can be launched.
 
 
 // ===================================================================================================================
-// Contact me if you have any scripts you would like to be reviewed for adding to the app. 
-// If you can keep these scripts as modular as possible so that users can integrate them into their
-// own projects - this would be the best solution for everyone.
-// This application will eventually be available on my Rosebud related site "RoseBoots".
-// Stay tuned for it's release - Updates will be be posted here.
-
+// Contact me if you have any custom shaders you have or would like and I can convert them for use in ThreeJs
+// and fix them if need be.
+// I'm also for hire if you would like me to make some custom shaders for your games. 
+// LordBoots on Discord is where you will find me, I am always active in the RoseBud channels. 
 
 
 
